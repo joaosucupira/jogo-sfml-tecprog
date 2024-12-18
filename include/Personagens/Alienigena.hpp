7a789d5c2bd0a031002b09f1663b19b55eb3152f
joao <jasucupira@gmail.com>
2024-12-18 03:24:40 -0300

    Supostamente finalizado inimigo

--- include/Personagens/Alienigena.hpp
+++ include/Personagens/Alienigena.hpp
@@ -4,6 +4,18 @@
 #include "Inimigo.hpp"
 #include "Jogador.hpp"
 
+// TROCAR PARA SPRITE DE ALIENIGENA
+#define ALIENIGENA_PATH "./assets/Jogador/player.24x24.png"
+
+#define TAM_SECAO 24
+#define QNTD_SECAO 3
+
+#define SEC_INICIAL_X 24
+#define SEC_INICIAL_Y 0
+
+#define SEC_FINAL_X 24
+#define SEC_FINAL_Y 48
+
 namespace Personagens{
 
     class Alienigena : public Inimigo {
@@ -12,11 +24,14 @@ namespace Personagens{
 
     public:
 
-        Alienigena();
+        Alienigena(const float x_inicial, const float y_inicial);
         ~Alienigena();
 
-        void danificar(Jogador* pJ){}
-        void executar(){}
+        void carregarFigura();
+
+        void danificar(Jogador* pJ) {}
+        void executar();
+        void salvaDataBuffer();
     };
 
 } using namespace Personagens;
